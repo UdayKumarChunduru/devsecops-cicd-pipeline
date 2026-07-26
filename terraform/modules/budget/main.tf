c31864f62a15a9ca5ed47fe9fb70c4209947b84c
Uday <forteciipher@gmail.com>
2026-07-26 23:49:21 +0530

    add aws budgets alert for cost visibility during testing
    sends an email at 50 percent, 80 percent, and a forecasted 100 percent of a configurable monthly limit, defaults to 50 dollars, nothing in the terraform-aws or terraform-aws-cloud branches previously told you if eks, ec2, nat gateway and efs billing was trending above expectations while iterating
    
    Signed-off-by: Uday <forteciipher@gmail.com>

---
+++ terraform/modules/budget/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = ">= 1.15.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+resource "aws_budgets_budget" "devsecops_pipeline" {
+  name         = "devsecops-pipeline-monthly"
+  budget_type  = "COST"
+  limit_amount = var.monthly_limit_usd
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  notification {
+    comparison_operator       = "GREATER_THAN"
+    threshold                 = 50
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "ACTUAL"
+    subscriber_email_addresses = [var.alert_email]
+  }
+
+  notification {
+    comparison_operator       = "GREATER_THAN"
+    threshold                 = 80
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "ACTUAL"
+    subscriber_email_addresses = [var.alert_email]
+  }
+
+  notification {
+    comparison_operator       = "GREATER_THAN"
+    threshold                 = 100
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "FORECASTED"
+    subscriber_email_addresses = [var.alert_email]
+  }
+}
---
+++ terraform/modules/budget/variables.tf
@@ -0,0 +1,8 @@
+variable "monthly_limit_usd" {
+  type    = string
+  default = "50"
+}
+
+variable "alert_email" {
+  type = string
+}
