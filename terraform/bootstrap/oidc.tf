d51867c02c1ed858107a2db230cc0628f092a04a
Uday <forteciipher@gmail.com>
2026-07-26 23:59:37 +0530

    remove unused caller identity data source in oidc bootstrap
    declared but never referenced, terraform/bootstrap/main.tf already has its own aws_caller_identity.current that oidc.tf could have reused if it needed the account id, this one had no consumer at all
    
    Signed-off-by: Uday <forteciipher@gmail.com>

--- terraform/bootstrap/oidc.tf
+++ terraform/bootstrap/oidc.tf
@@ -3,8 +3,6 @@ variable "github_org_repo" {
   default = "UdayKumarChunduru/devsecops-cicd-pipeline"
 }
 
-data "aws_caller_identity" "bootstrap" {}
-
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
